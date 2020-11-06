3e0ca4c49eb146dbc4af23eb76afacb0d528f94f
evgeny.konoplev <HorDex994>
2020-11-06 13:37:35 +0300

    rebuild

---
+++ middle/public/js/about.6947cde0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{ca81:function(n,t,e){"use strict";e.r(t);var u=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[n._v("About")])},a=[],c=e("2877"),l={},o=Object(c["a"])(l,u,a,!1,null,null,null);t["default"]=o.exports}}]);
+//# sourceMappingURL=about.6947cde0.js.map
